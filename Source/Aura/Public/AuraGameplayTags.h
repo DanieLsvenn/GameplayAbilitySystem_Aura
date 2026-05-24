e3160a868d04d66cf2cb8352ca25835186dc98d7
Nguyen Nhu Thanh <seizurebabe@gmail.com>
2026-05-24 17:44:09 +0700

    Input Config Data Asset

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -1,4 +1,4 @@
-
+// Copyright Druid Mechanics
 
 #pragma once
 
@@ -7,22 +7,21 @@
 
 /**
  * AuraGameplayTags
- * 
+ *
  * Singleton containing native Gameplay Tags
  */
 
 struct FAuraGameplayTags
 {
 public:
-	
-	static const FAuraGameplayTags& Get() { return GameplayTags; }
+	static const FAuraGameplayTags& Get() { return GameplayTags;}
 	static void InitializeNativeGameplayTags();
-	
+
 	FGameplayTag Attributes_Primary_Strength;
 	FGameplayTag Attributes_Primary_Intelligence;
 	FGameplayTag Attributes_Primary_Resilience;
 	FGameplayTag Attributes_Primary_Vigor;
-	
+
 	FGameplayTag Attributes_Secondary_Armor;
 	FGameplayTag Attributes_Secondary_ArmorPenetration;
 	FGameplayTag Attributes_Secondary_BlockChance;
@@ -33,12 +32,13 @@ public:
 	FGameplayTag Attributes_Secondary_ManaRegeneration;
 	FGameplayTag Attributes_Secondary_MaxHealth;
 	FGameplayTag Attributes_Secondary_MaxMana;
-	
-protected:
-	
-	
+
+	FGameplayTag InputTag_LMB;
+	FGameplayTag InputTag_RMB;
+	FGameplayTag InputTag_1;
+	FGameplayTag InputTag_2;
+	FGameplayTag InputTag_3;
+	FGameplayTag InputTag_4;
 private:
-	
 	static FAuraGameplayTags GameplayTags;
-	
 };
